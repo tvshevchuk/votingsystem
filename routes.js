4d974e5e5294cd65680a8010a640b59516840459
tvshevchuk <tvshevchuk@gmail.com>
2015-01-26 18:59:08 +0200

    v

--- routes.js
+++ routes.js
@@ -53,6 +53,7 @@ module.exports = function(app, passport) {
     });
 
     function isLoggedIn(req, res, next) {
+        console.log('is authenticated', req.isAuthenticated());
         if (req.isAuthenticated()) {
             return next();
         };
