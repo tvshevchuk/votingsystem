9bc6ba8d70f5200ef8c59bba9ae8cc015dcf336f
Taras <tvshevchuk@gmail.com>
2016-01-29 01:35:30 +0200

    req.headers["x-forwarded-proto"] !== "https"

--- routes.js
+++ routes.js
@@ -9,7 +9,7 @@ var User = require('./models/User.js');
 var router = express.Router();
 
 router.use(function (req, res, next) {
-    if (!req.secure) {
+    if (req.headers["x-forwarded-proto"] !== "https") {
         return res.redirect('https://' + req.headers.host + req.url);
     }
     next();
